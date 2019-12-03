1a6deb9abae6644c61db17806ba6593358628534
Nicolas Audinet <nicolas.audinet@relexsolutions.com>
2019-12-03 10:51:10 +0200

    Day 1 Solution 1 & 2

--- day1/Main.hs
+++ day1/Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
+import Data.List (unfoldr)
+
+moduleFuel :: Int -> Int
+moduleFuel x = (x `div` 3) - 2
+
+fuelFuel :: Int -> Int
+fuelFuel = sum . unfoldr fuel
+  where
+    fuel :: Int -> Maybe (Int, Int)
+    fuel x =
+      let y = moduleFuel x
+      in if y <= 0 then Nothing else Just (y, y)
+
+totalFuel :: Int -> Int
+totalFuel x = moduleFuel x + fuelFuel (moduleFuel x)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  modules <- map read . lines <$> readFile "input"
+  print (sum $ map moduleFuel modules)
+  print (sum $ map totalFuel modules)
