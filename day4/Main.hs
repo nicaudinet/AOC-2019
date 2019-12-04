1ba7a2988c8f186573c3ecc9b4cc4a877a4bac2e
nicaudinet <nic.audinet27@gmail.com>
2019-12-05 00:37:58 +0200

    Day 4 Solution 1 & 2

--- day4/Main.hs
+++ day4/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import Data.List (sort, group)
+
+input :: [Int]
+input = [134564 .. 585159]
+
+hasDoubleDigit :: Int -> Bool
+hasDoubleDigit = not . null . filter (> 1) . map length . group . show
+
+alwaysIncreasing :: Int -> Bool
+alwaysIncreasing n = let n' = show n in sort n' == n'
+
+hasOnlyDoubleDigit :: Int -> Bool
+hasOnlyDoubleDigit = not . null . filter (== 2) . map length . group . show
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  print (length [ x | x <- input, alwaysIncreasing x, hasDoubleDigit x ])
+  print (length [ x | x <- input, alwaysIncreasing x, hasOnlyDoubleDigit x ])
