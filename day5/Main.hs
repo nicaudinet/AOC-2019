ece686463649948a27c08553a19784688ea24bca
Nicolas Audinet <nicolas.audinet@relexsolutions.com>
2019-12-11 13:53:37 +0200

    wip

--- day5/Main.hs
+++ day5/Main.hs
@@ -1,12 +1,11 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
 import Control.Monad ((<=<))
 import Data.List (find, splitAt)
 import Data.Vector (Vector, fromList, (!), (//))
 import GHC.Stack
-
-type Memory = Vector Int
-type PC = Int
 
 data Mode
   = Position
@@ -21,57 +20,89 @@ data Instr
   | Mul Value Value Int
   | Input Int
   | Output Value
+  | JumpIfTrue Value Value
+  | JumpIfFalse Value Value
+  | LessThan Value Value Int
+  | Equals Value Value Int
   | Halt
+
+type Memory = Vector Int
+type PC = Int
+type Modes = (Mode, Mode)
 
 -- | Parsing the instructions
 
 parseContents :: HasCallStack => String -> [Int]
 parseContents str = read ("[" <> init str <> "]")
 
-parseInstr :: HasCallStack => Memory -> PC -> (Instr, PC)
+parseInstr :: HasCallStack => Memory -> PC -> Instr
 parseInstr mem pc = 
   let 
       opInt = show (mem ! pc)
-      (modes, op) = splitAt (length opInt - 2) opInt
+      (rest, op) = splitAt (length opInt - 2) opInt
+      modes = parseModes rest
   in  
     case read op of
-      1  -> parseAdd    modes mem pc
-      2  -> parseMul    modes mem pc
-      3  -> parseInput        mem pc
+      1  -> parseAdd modes mem pc
+      2  -> parseMul modes mem pc
+      3  -> parseInput mem pc
       4  -> parseOutput modes mem pc
-      99 -> (Halt, error "PC was called after Halt instruction")
-      o  -> error ("Op \"" <> op <> "\" failed to parse (it is not one of 1, 2, 3, 4, 99)")
+      5  -> parseJumpIfTrue modes mem pc
+      6  -> parseJumpIfFalse modes mem pc
+      7  -> parseLessThan modes mem pc
+      8  -> parseEquals modes mem pc
+      99 -> Halt
+      o  -> error ("Op \"" <> op <> "\" failed to parse (it is not one of 1, 2, 3, 4, 5, 6, 7, 8, 99)")
 
-parseAdd :: String -> Memory -> PC -> (Instr, PC)
-parseAdd modes mem pc =
-  let (m1, m2) = parseModes modes
-      i1  = mem ! (pc + 1)
-      i2  = mem ! (pc + 2)
-      out = mem ! (pc + 3)
-      instr = Add (Value i1 m1) (Value i2 m2) out
-  in (instr, pc + 4)
+parseAdd :: Modes -> Memory -> PC -> Instr
+parseAdd (m1, m2) mem pc =
+  let i1   = mem ! pc + 1
+      i2   = mem ! pc + 2
+      dest = mem ! pc + 3
+  in Add (Value i1 m1) (Value i2 m2) dest
 
-parseMul :: String -> Memory -> PC -> (Instr, PC)
-parseMul modes mem pc =
-  let (m1, m2) = parseModes modes
-      i1  = mem ! (pc + 1)
-      i2  = mem ! (pc + 2)
-      out = mem ! (pc + 3)
-      instr = Mul (Value i1 m1) (Value i2 m2) out
-  in (instr, pc + 4)
+parseMul :: Modes -> Memory -> PC -> Instr
+parseMul (m1, m2) mem pc =
+  let i1   = mem ! pc + 1
+      i2   = mem ! pc + 2
+      dest = mem ! pc + 3
+  in Mul (Value i1 m1) (Value i2 m2) dest
 
-parseInput :: Memory -> PC -> (Instr, PC)
+parseInput :: Memory -> PC -> Instr
 parseInput mem pc =
-  let dest = mem ! (pc + 1)
-      instr = Input dest
-  in (instr, pc + 2)
+  let dest = mem ! pc + 1
+  in Input dest
 
-parseOutput :: String -> Memory -> PC -> (Instr, PC)
-parseOutput modes mem pc =
-  let (m1, _) = parseModes modes
-      toPrint = mem ! (pc + 1)
-      instr = Output (Value toPrint m1)
-  in (instr, pc + 2)
+parseOutput :: Modes -> Memory -> PC -> Instr
+parseOutput (m1, _) mem pc =
+  let toPrint = mem ! pc + 1
+  in Output (Value toPrint m1)
+
+parseJumpIfTrue :: Modes -> Memory -> PC -> Instr
+parseJumpIfTrue (m1, m2) mem pc =
+  let cond = mem ! pc + 1
+      dest = mem ! pc + 2
+  in JumpIfTrue (Value cond m1) (Value dest m2)
+
+parseJumpIfFalse :: Modes -> Memory -> PC -> Instr
+parseJumpIfFalse (m1, m2) mem pc =
+  let cond = mem ! pc + 1
+      dest = mem ! pc + 2
+  in JumpIfFalse (Value cond m1) (Value dest m2)
+
+parseLessThan :: Modes -> Memory -> PC -> Instr
+parseLessThan (m1, m2) mem pc =
+  let i1   = mem ! pc + 1
+      i2   = mem ! pc + 2
+      dest = mem ! pc + 3
+  in LessThan (Value i1 m1) (Value i2 m2) dest
+
+parseEquals :: Modes -> Memory -> PC -> Instr
+parseEquals (m1, m2) mem pc =
+  let i1   = mem ! pc + 1
+      i2   = mem ! pc + 2
+      dest = mem ! pc + 3
+  in Equals (Value i1 m1) (Value i2 m2) dest
 
 parseMode :: HasCallStack => Char -> Mode
 parseMode '0' = Position
@@ -87,52 +118,92 @@ parseModes modes    = error ("Modes \"" <> modes <> "\" failed to parse (string 
 -- | Running the program
 
 compute :: HasCallStack => PC -> Memory -> IO Memory
-compute pc mem = do
-  let (instr, pc') = parseInstr mem pc
-  maybe (pure mem) (compute pc') =<< eval instr mem
+compute pc mem = 
+  eval mem pc >>= \case
+    (pc', Just mem') -> compute pc' mem'
+    (_, Nothing)     -> pure mem
 
-eval :: HasCallStack => Instr -> Memory -> IO (Maybe Memory)
-eval instr =
-  case instr of
-    Add v1 v2 out -> add v1 v2 out
-    Mul v1 v2 out -> mul v1 v2 out
-    Input dest    -> input dest
-    Output val    -> output val
-    Halt          -> halt
+eval :: HasCallStack => Memory -> PC -> IO (PC, Maybe Memory)
+eval mem pc =
+  case parseInstr mem pc of
+    Add v1 v2 out    -> add v1 v2 out mem pc
+    Mul v1 v2 out    -> mul v1 v2 out mem pc
+    Input dest       -> input dest mem pc
+    Output val       -> output val mem pc
+    JumpIfTrue c d   -> jumpIfTrue c d mem pc
+    JumpIfFalse c d  -> jumpIfFalse c d mem pc
+    LessThan v1 v2 o -> lessThan v1 v2 o mem pc
+    Equals v1 v2 o   -> equals v1 v2 o mem pc
+    Halt             -> halt
 
 fetch :: HasCallStack => Value -> Memory -> Int
 fetch (Value addr Position) mem = mem ! addr
 fetch (Value val Immediate) _ = val
 
-add :: HasCallStack => Value -> Value -> Int -> Memory -> IO (Maybe Memory)
-add v1 v2 out mem = do
+add :: HasCallStack => Value -> Value -> Int -> Memory -> PC -> IO (PC, Maybe Memory)
+add v1 v2 dest mem pc = do
   let i1 = fetch v1 mem
       i2 = fetch v2 mem
-  pure (Just $ mem // [(out, i1 + i2)])
+      mem' = Just (mem // [(dest, i1 + i2)])
+  pure (pc + 4, mem')
     
-
-mul :: HasCallStack => Value -> Value -> Int -> Memory -> IO (Maybe Memory)
-mul v1 v2 out mem = do
+mul :: HasCallStack => Value -> Value -> Int -> Memory -> PC -> IO (PC, Maybe Memory)
+mul v1 v2 dest mem pc = do
   let i1 = fetch v1 mem
       i2 = fetch v2 mem
-  pure (Just $ mem // [(out, i1 * i2)])
+      mem' = Just (mem // [(dest, i1 * i2)])
+  pure (pc + 4, mem')
 
-input :: HasCallStack => Int -> Memory -> IO (Maybe Memory)
-input dest mem = do
+input :: HasCallStack => Int -> Memory -> PC -> IO (PC, Maybe Memory)
+input dest mem pc = do
   input <- read <$> getLine
-  pure (Just $ mem // [(dest, input)])
+  let mem' = Just (mem // [(dest, input)])
+  pure (pc + 2, mem')
 
-output :: HasCallStack => Value -> Memory -> IO (Maybe Memory)
-output val mem = do
+output :: HasCallStack => Value -> Memory -> PC -> IO (PC, Maybe Memory)
+output val mem pc = do
   print (fetch val mem)
-  pure (Just mem)
+  let mem' = Just mem
+  pure (pc + 2, mem')
 
-halt :: HasCallStack => Memory -> IO (Maybe Memory)
-halt = const (pure Nothing)
+jumpIfTrue :: Value -> Value -> Memory -> PC -> IO (PC, Maybe Memory)
+jumpIfTrue c d mem pc = do
+  let cond = fetch c mem
+      dest = fetch d mem
+  pure $ if cond /= 0 then (dest, Just mem) else (pc + 3, Just mem)
+
+jumpIfFalse :: Value -> Value -> Memory -> PC -> IO (PC, Maybe Memory)
+jumpIfFalse c d mem pc = do
+  let cond = fetch c mem
+      dest = fetch d mem
+  pure $ if cond == 0 then (dest, Just mem) else (pc + 3, Just mem)
+
+lessThan :: Value -> Value -> Int -> Memory -> PC -> IO (PC, Maybe Memory)
+lessThan v1 v2 dest mem pc = do
+  let i1 = fetch v1 mem
+      i2 = fetch v2 mem
+      mem' = Just (mem // [(dest, if i1 <= i2 then 1 else 0)])
+  pure (pc + 4, mem')
+
+equals :: Value -> Value -> Int -> Memory -> PC -> IO (PC, Maybe Memory)
+equals v1 v2 dest mem pc = do
+  let i1 = fetch v1 mem
+      i2 = fetch v2 mem
+      mem' = Just (mem // [(dest, if i1 == i2 then 1 else 0)])
+  pure (pc + 4, mem')
+
+halt :: HasCallStack => IO (PC, Maybe Memory)
+halt = pure (error "Tried to call PC after Halt instruction", Nothing)
 
 main :: HasCallStack => IO ()
 main = do
   contents <- parseContents <$> readFile "input"
   let mem = fromList contents
   compute 0 mem
-  print "Done"
+  pure ()
+
+test :: IO ()
+test = do 
+  let mem = fromList [3,9,8,9,10,9,4,9,99,-1,8]
+  compute 0 mem
+  pure ()
