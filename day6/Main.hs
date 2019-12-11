3064ab96540a445f3518f8b60e6d0ee5a543f88c
Nicolas Audinet <nic.audinet27@gmail.com>
2019-12-12 00:16:26 +0200

    Day 6 Problem 1

--- day6/Main.hs
+++ day6/Main.hs
@@ -1,4 +1,26 @@
 module Main where
 
+import Data.List (break)
+import Data.Bifunctor (second)
+
+parseOrbits :: String -> [(String, String)]
+parseOrbits = map (second tail . break (== ')')) . lines
+
+next :: [(String, String)] -> [String] -> [String]
+next orbits curr = map snd (filter (flip elem curr . fst) orbits)
+
+fix :: Eq a => (a -> a) -> a -> [a]
+fix f a =
+  let result = f a
+  in a : if result == a then [] else fix f result
+
+orbitTree :: [(String, String)] -> [[String]]
+orbitTree orbits = fix (next orbits) ["COM"]
+
+countOrbits :: [[String]] -> Int
+countOrbits = sum . map (\(i, os) -> i * length os) . zip [0..]
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  orbits <- parseOrbits <$> readFile "input"
+  print (countOrbits $ orbitTree orbits)
