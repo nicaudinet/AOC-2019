7c3914da9a36a2c450ff064eafb1b2db886c4ad7
Nicolas Audinet <nicolas.audinet@relexsolutions.com>
2019-12-11 13:04:20 +0200

    Day 5 Part 1

--- day5/Main.hs
+++ day5/Main.hs
@@ -3,7 +3,6 @@ module Main where
 import Control.Monad ((<=<))
 import Data.List (find, splitAt)
 import Data.Vector (Vector, fromList, (!), (//))
-import Debug.Trace
 import GHC.Stack
 
 type Memory = Vector Int
@@ -12,8 +11,11 @@ type PC = Int
 data Mode
   = Position
   | Immediate
+  deriving Show
 
 data Value = Value Int Mode
+  deriving Show
+
 data Instr
   = Add Value Value Int
   | Mul Value Value Int
@@ -77,19 +79,19 @@ parseMode '1' = Immediate
 parseMode c   = error ("Mode \"" <> [c] <> "\" failed to parse (is not one of '0', '1')")
 
 parseModes :: HasCallStack => String -> (Mode, Mode)
-parseModes []         = (   Position,    Position)
-parseModes (x:[])     = (parseMode x,    Position)
-parseModes (x:y:[])   = (parseMode x, parseMode y)
-parseModes modes      = error ("Modes \"" <> modes <> "\" failed to parse (string is likely too long)")
+parseModes []       = (   Position,    Position)
+parseModes (x:[])   = (parseMode x,    Position)
+parseModes (x:y:[]) = (parseMode y, parseMode x)
+parseModes modes    = error ("Modes \"" <> modes <> "\" failed to parse (string is likely too long)")
 
 -- | Running the program
 
 compute :: HasCallStack => PC -> Memory -> IO Memory
 compute pc mem = do
   let (instr, pc') = parseInstr mem pc
-  compute pc' =<< eval instr mem
+  maybe (pure mem) (compute pc') =<< eval instr mem
 
-eval :: HasCallStack => Instr -> Memory -> IO Memory
+eval :: HasCallStack => Instr -> Memory -> IO (Maybe Memory)
 eval instr =
   case instr of
     Add v1 v2 out -> add v1 v2 out
@@ -99,41 +101,38 @@ eval instr =
     Halt          -> halt
 
 fetch :: HasCallStack => Value -> Memory -> Int
-fetch (Value addr Position) mem = trace ("fetch: " <> show addr) $ mem ! addr
+fetch (Value addr Position) mem = mem ! addr
 fetch (Value val Immediate) _ = val
 
-add :: HasCallStack => Value -> Value -> Int -> Memory -> IO Memory
+add :: HasCallStack => Value -> Value -> Int -> Memory -> IO (Maybe Memory)
 add v1 v2 out mem = do
   let i1 = fetch v1 mem
       i2 = fetch v2 mem
-  pure (mem // [(out, i1 + i2)])
+  pure (Just $ mem // [(out, i1 + i2)])
     
 
-mul :: HasCallStack => Value -> Value -> Int -> Memory -> IO Memory
+mul :: HasCallStack => Value -> Value -> Int -> Memory -> IO (Maybe Memory)
 mul v1 v2 out mem = do
   let i1 = fetch v1 mem
       i2 = fetch v2 mem
-  pure (mem // [(out, i1 * i2)])
+  pure (Just $ mem // [(out, i1 * i2)])
 
-input :: HasCallStack => Int -> Memory -> IO Memory
+input :: HasCallStack => Int -> Memory -> IO (Maybe Memory)
 input dest mem = do
   input <- read <$> getLine
-  pure (mem // [(dest, input)])
+  pure (Just $ mem // [(dest, input)])
 
-output :: HasCallStack => Value -> Memory -> IO Memory
+output :: HasCallStack => Value -> Memory -> IO (Maybe Memory)
 output val mem = do
   print (fetch val mem)
-  pure mem
+  pure (Just mem)
 
-halt :: HasCallStack => Memory -> IO Memory
-halt = pure
+halt :: HasCallStack => Memory -> IO (Maybe Memory)
+halt = const (pure Nothing)
 
 main :: HasCallStack => IO ()
 main = do
   contents <- parseContents <$> readFile "input"
-  putStr "Length of contents: "
-  print (length contents)
-  print contents
   let mem = fromList contents
   compute 0 mem
   print "Done"
